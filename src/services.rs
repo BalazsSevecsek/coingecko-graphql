053314d731565f69c8a550037d99e3b26b6dbf4c
Balazs Sevecsek <balazs.sevecsek@gmail.com>
2023-10-16 00:08:44 +0200

    delete not needed service file- subscription.

--- src/services.rs
+++ src/services.rs
@@ -1,10 +1,8 @@
-mod current_price_subscription_service;
 mod get_crypto_ids_service;
 mod get_current_price_service;
 mod get_historical_data_service;
 mod symbol_cache_service;
 
-pub use current_price_subscription_service::subscribe_to_current_price;
 pub use get_crypto_ids_service::get_crypto_ids_service;
 pub use get_current_price_service::get_current_price_service;
 pub use get_historical_data_service::get_historical_data_service;
